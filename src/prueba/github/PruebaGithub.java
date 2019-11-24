4ae216747fa198856db4c343d285512c13f0709e
raul <raulvaque99@hotmail.com>
2019-11-24 11:45:36 +0100

    prueba para gihub

--- src/prueba/github/PruebaGithub.java
+++ src/prueba/github/PruebaGithub.java
@@ -17,7 +17,7 @@ public class PruebaGithub {
     public static void main(String[] args) {
         
         
-        System.out.println("prueba github");
+        System.out.println("prueba  github");
     }
     
 }
